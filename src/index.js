2d8a93b35d0a390b095ad9f4424094f00a2cd423
Ethan Hannum <ethanhannum10@gmail.com>
2026-07-29 14:46:31 -0700

    Make menu button delete page content
    The navigation buttons must be able to delete all page #content and then
    display new elements in the #content when clicked. The menu button will
    now remove all elements in #content. The IIFE of "building" will
    set stringPage to "homePage", as that is the page that first loads.
    building.swapMenu is called when the menu button is clicked and will
    check what is current stringPage is, if it's homePage it will then
    delete homePage elements, set stringPage to "menuPage" and then display
    the menu page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import "./styles.css";
 import { homePage } from "./homePage.js";
 
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+
+const building = (() => {
+    let stringPage = "homePage";
+
+    const swapMenu = () => {
+        if (stringPage === "homePage") {
+            homePage.destroy();
+        }
+        stringPage = "menuPage";
+        console.log(stringPage);
+    }
+
+    return { swapMenu };
+})();
+
+menu.addEventListener("click", building.swapMenu);
+
 homePage.build();
 
 console.log("Webpack is working!");
