42ef65e292d2df8e14a0c00580300508de7b8930
Ethan Hannum <ethanhannum10@gmail.com>
2026-08-06 18:36:09 -0700

    Make site fully dynamic and working
    The last step needed to finish the website. When each button is clicked
    it should "destroy" the current page and then load the desired page.
    Done through the building IIFE, which first initializes stringPage
    assigning it to "#homePage". Then whenever a swap is called, the swap
    method will destroy the current page, build the new page then set
    stringPage to the newly loaded page. Now the website is fully dynamic!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { homePage } from "./homePage.js";
 import { menuPage } from "./menuPage.js";
+import { aboutPage } from "./aboutPage.js";
 
 const home = document.querySelector("#home");
 const menu = document.querySelector("#menu");
@@ -9,17 +10,44 @@ const about = document.querySelector("#about");
 const building = (() => {
     let stringPage = "homePage";
 
-    const swapMenu = () => {
+    const destroyPage = () => {
         if (stringPage === "homePage") {
             homePage.destroy();
+        } else if (stringPage === "menuPage") {
+            menuPage.destroy();
+        } else if (stringPage === "aboutPage") {
+            aboutPage.destroy();
         }
-        stringPage = "menuPage";
-        console.log(stringPage);
     }
 
-    return { swapMenu };
+    const swapHome = () => {
+        destroyPage();
+        stringPage = "homePage";
+        console.log(stringPage);
+        homePage.build();
+    }
+
+    const swapMenu = () => {
+        destroyPage();
+        stringPage = "menuPage";
+        console.log(stringPage);
+        menuPage.build();
+    }
+
+    const swapAbout = () => {
+        destroyPage();
+        stringPage = "aboutPage";
+        console.log(stringPage);
+        aboutPage.build();
+    }
+
+    return { swapHome, swapMenu, swapAbout };
 })();
 
+homePage.build();
+
+home.addEventListener("click", building.swapHome);
 menu.addEventListener("click", building.swapMenu);
+about.addEventListener("click", building.swapAbout);
 
 console.log("Webpack is working!");
